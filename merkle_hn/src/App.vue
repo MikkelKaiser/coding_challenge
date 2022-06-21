63f0e843d7353e1eb7fcacb928c317c90ed85110
Mikkel Kaiser <mikkel@adnami.io>
2022-06-21 12:26:00 +0200

    Setting up project, and adding nav-bar component

---
+++ merkle_hn/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <NavMenu></NavMenu>
+</template>
+
+<script>
+
+import NavMenu from '@/components/Navbar';
+export default {
+  name: 'App',
+  components: {
+    NavMenu,
+  }
+}
+</script>
+
+<style>
+
+</style>
